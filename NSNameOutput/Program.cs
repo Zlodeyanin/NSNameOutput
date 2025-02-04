626246094145b75fd9b44c482e2504e90d120582
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-02-04 03:18:43 +0300

    Добавьте файлы проекта.

---
+++ NSNameOutput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NSNameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int additionalSymbolsCount = 2;
+            
+            Console.Write("Введите желаемый символ: ");
+            char userSymbol = Convert.ToChar(Console.ReadLine());
+            Console.Write("Введите ваше имя: ");
+            string userName = Console.ReadLine();
+
+            for (int i = 0; i < userName.Length + additionalSymbolsCount; i++)
+            {
+                Console.Write(userSymbol);
+            }
+            Console.WriteLine($"\n{userSymbol}{userName}{userSymbol}");
+
+            for (int i = 0; i < userName.Length + additionalSymbolsCount; i++)
+            {
+                Console.Write(userSymbol);
+            }
+            Console.ReadKey();
+        }
+    }
+}
